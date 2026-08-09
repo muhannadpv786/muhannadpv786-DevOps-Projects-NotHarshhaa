ccb695a8305203f779f211ab309d3496b4600f81
MUHANNED PV <p.vmuhannad786@gmail.com>
2026-08-09 20:53:57 +0530

    Delete DevOps-Project-01/infrastructure/bastion directory

--- DevOps-Project-01/infrastructure/bastion/output.tf
+++
@@ -1,9 +0,0 @@
-output "bastion_public_ip" {
-  description = "Public IP of the bastion host"
-  value       = aws_instance.bastion.public_ip
-}
-
-output "bastion_instance_id" {
-  description = "Instance ID of the bastion host"
-  value       = aws_instance.bastion.id
-}
