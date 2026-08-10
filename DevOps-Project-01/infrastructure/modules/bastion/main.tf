9c383478f62b2ca46a5ed1bf69627e38ebe2b912
MUHANNED PV <p.vmuhannad786@gmail.com>
2026-08-10 12:39:02 +0530

    Update main.tf

--- DevOps-Project-01/infrastructure/modules/bastion/main.tf
+++ DevOps-Project-01/infrastructure/modules/bastion/main.tf
@@ -1,22 +1,24 @@
 # Bastion Host Module
-
-data "aws_ami" "amazon_linux" {
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 }
 
 resource "aws_instance" "bastion" {
-  ami                         = data.aws_ami.amazon_linux.id
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = var.key_name
   subnet_id                   = var.public_subnet_id
